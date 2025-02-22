09a840aa4d8ed16b3a327b062de62251ddfd956c
WeerasingheMSC <sahanchampathi2001ugc@gmail.com>
2025-02-22 15:41:42 +0530

    fully created backend

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require('./moduls/product.module')
+const productRoute = require("./routes/product.routes.js")
 const app = express();
 
-const PORT = process.env.PORT||5000;
+// middleware
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-
-app.get('/',(req, res) =>{
-    res.send('hello world!')
-});
-
-app.listen(PORT,() =>{
-    console.log(`Server is running on port ${PORT}`);
-});
+//routes
+app.use("/api/products",productRoute);
 
 if (!MONGO_URI) {
-    console.error("❌ MONGO_URI is missing! Please set it in the .env file.");
-    process.exit(1); // Exit the application if MONGO_URI is not set
+  console.error("❌ MONGO_URI is missing! Please set it in the .env file.");
+  process.exit(1); // Exit the application if MONGO_URI is not set
 }
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI)
-    .then(() => {
-        console.log('✅ Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.error('❌ MongoDB connection error:', err);
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+  });
